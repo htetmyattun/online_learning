c46e90827db19597fd84a266c74cfb13f3e8e63b
htetmyattun <54614442+htetmyattun@users.noreply.github.com>
2020-05-13 13:35:09 -0700

    Update studentController.php

--- app/Http/Controllers/studentController.php
+++ app/Http/Controllers/studentController.php
@@ -36,12 +36,12 @@ class studentController extends Controller
                 ->get();
 
         $first_course=Course::leftJoin('lecturers', 'courses.lecturer_id', '=', 'lecturers.id')
-<<<<<<< HEAD
+
                 ->select('courses.name as cname', 'lecturers.name as lecturer_name','courses.price as price','courses.discount_price as discount_price','courses.photo as photo','courses.id as id')
                 ->orderBy('courses.created_at','DESC')
                 ->first();
         return view('student.pages.home',['first_course'=>$first_course],['courses' => $courses]);
-=======
+
                     ->leftJoin('student_course',function($join){
                     $join->on('student_course.course_id','=','courses.id')
                          ->where('student_course.student_id','=',Auth::id());
@@ -51,7 +51,6 @@ class studentController extends Controller
                     ->first();
         
         return view('student.pages.home',['first_course'=>$first_course],['courses' => $courses]);
->>>>>>> b3cadb19bb98675f61e6faeb1be715666dbbea2e
     }
     public function detail_course($id)
     {
